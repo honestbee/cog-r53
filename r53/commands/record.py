dfb789a5d9447e61e6f13033a7be18a4f1976259
Vincent De Smet <vincent.desmet@honestbee.com>
2017-04-22 20:49:54 +0800

    Implement record list and bump version

--- r53/commands/record.py
+++ r53/commands/record.py
@@ -5,8 +5,48 @@ import boto3
 class Record(Route53Base):
   def __init__(self):
     super().__init__()
-    client = boto3.client('route53')
+    self.r53client = boto3.client('route53')
 
   def run(self):
     handler = self.parse_subcommand_()
     handler()
+
+  def list(self):
+    results = []
+    zones = self.request.options['zone']
+    # zones can be string or tuple
+    for zone in zones if not isinstance(zones, str) else [zones]:
+      response = self.r53client.list_resource_record_sets(
+        HostedZoneId=zone,
+        MaxItems='100')
+      self.parse_records(response, zone, results)
+      while response["IsTruncated"]:
+        response = self.r53client.list_resource_record_sets(
+          HostedZoneId=zone,
+          StartRecordName=response["NextRecordName"],
+          MaxItems='100')
+        self.parse_records(response, zone, results)
+    self.response.content(results, template="records_list").send()
+
+  # parse response into result object
+  def parse_records(self, response, zone, results):
+    for r in response["ResourceRecordSets"]:
+        values = []
+        if "ResourceRecords" in r.keys():
+            for v in r["ResourceRecords"]:
+              values.append(v["Value"])
+        zone = {
+          "Zone":zone,
+          "Name":r["Name"],
+          "Type":r["Type"],
+          "AliasTarget":r["AliasTarget"] if "AliasTarget" in r.keys() else None,
+          "ResourceRecords": ",".join(values)
+        }
+        results.append(zone)
+
+  def parse_subcommand_(self):
+    if self.request.args == None:
+      return self.list
+    if self.request.args[0] == "list":
+      return self.list
+    self.fail("Unknown subcommand: '%s'" % self.request.args[0])
--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup (
     name = "r53",
-    version = "0.0.2",
+    version = "0.1.1",
     description = "Cog commands for AWS Route53",
     author = "Vincent De Smet",
     author_email = "vincent.desmet@honestbee.com",
